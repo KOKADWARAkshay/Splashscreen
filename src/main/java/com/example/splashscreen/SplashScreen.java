a46711de3a3af2aee42cb46d8a1dc1afc64e0afc
Akshay Kokadwar <70112790+KOKADWARAkshay@users.noreply.github.com>
2021-01-26 18:30:53 +0530

    Splashscreen for android app

---
+++ src/main/java/com/example/splashscreen/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 6000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity2.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        } , SPLASH_TIME_OUT);
+
+
+    }
+}
